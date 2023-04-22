b7701991947e6844d9d51f17c3f9f2298ec98d32
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-22 12:06:48 -0300

    Update NavBar.jsx
    Se hizo una Nav provisoria

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+import styles from "./NavBar.module.css";
+
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <nav className={styles.container}>
+      <p>LOGO</p>
 
-export default NavBar
+      <div className={styles.buttonsNav}>
+        <Link to="/home" className={styles.linksNav}>
+          Home
+        </Link>
+        <Link to="/home" className={styles.linksNav}>
+          Shop
+        </Link>
+        <Link className={styles.linksNav}>Contactos</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
