a7047de80eddd9fec107f6540e13aef3143b89b9
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-22 12:05:56 -0300

    Update App.js
    Se agregó los estilos en App

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from './views/Home/Home';
-import Landing from './views/Lading/Landing';
+import Home from "./views/Home/Home";
+import Landing from "./views/Lading/Landing";
+import NavBar from "./components/NavBar/NavBar";
 
 const App = () => {
   return (
     <>
-    <Routes>
-        <Route path="/home" element={<Home />}/>
-        <Route path="/landing" element={<Landing />}/>
-    </Routes>
+      <NavBar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/landing" element={<Landing />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
 export default App;
