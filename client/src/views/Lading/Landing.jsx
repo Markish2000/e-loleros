35adef0544f0df43474f22269e8cf1f11ab0f16a
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-22 10:41:44 -0300

    Create Landing.jsx
    Se creó el componente de Landing

---
+++ client/src/views/Lading/Landing.jsx
@@ -0,0 +1,16 @@
+const Landing = () => {
+  const marcosHabla = "";
+  return (
+    <div>
+      <p>Soy el Landing</p>
+      <h2>length ver si aparezco</h2>
+    </div>
+  );
+};
+
+export default Landing;
+
+// ??
+// !!
+// TODO
+// *
