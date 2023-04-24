9b15c3c67de09943ab67749f91b84a57af1bf874
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-24 11:58:37 -0300

    Create index.jsx
    Landing component

---
+++ client/src/views/landingPage/index.jsx
@@ -0,0 +1,40 @@
+import styles from './Landing.module.css';
+import { Link } from 'react-router-dom';
+
+const LandingPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.divTitle}>
+        <h1 className={styles.titleH1}>E-Loleros</h1>
+        <h2>League of Legends</h2>
+        {/* <Link to="/home" className={styles.linkHome}>
+          Home
+        </Link> */}
+      </div>
+      <div className={styles.presentation}>
+        <h3>Bienvenidos</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem expedita facere blanditiis veritatis numquam, vero,
+          dolor, laboriosam amet iste fugit dicta repudiandae maxime ipsa quis?
+          Eum esse accusamus voluptas sint!
+        </p>
+        <iframe
+          className={styles.video}
+          src='https://www.youtube.com/embed/kmNui6kC1Lk'
+          title='YouTube video player'
+          frameborder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
+
+// ??
+// !!
+// TODO
+// *
