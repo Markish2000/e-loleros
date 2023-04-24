011095f2ccb9d8f1ab7b718f37bf0ac0e75978fd
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-24 11:59:26 -0300

    Update App.js
    Se modificó nombre de las carpetas

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import HomePage from './views/homePage';
-import LandingPage from './views/landingPage';
+import HomePage from './views/HomePage';
+import LandingPage from './views/LandingPage';
 import NavBar from './components/NavBar';
 
 const App = () => {
