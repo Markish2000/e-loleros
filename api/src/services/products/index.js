195001074283110be352e3d2388ecdb8c5b7fca6
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-04-24 20:21:49 -0300

    Se terminó el CRUD con su documentación de los productos

--- api/src/services/products/index.js
+++ api/src/services/products/index.js
@@ -57,9 +57,29 @@ class ProductsService {
   }
 
   //* Editar un producto.
-  async update() {
-    const response = 'Hola esto funciona y estoy en el service';
-    return response;
+  async update(
+    id,
+    { stock, availability, title, price, detail, mainImage, images }
+  ) {
+    const updateProduct = await productsModel.findByPk(id);
+    if (updateProduct === null) {
+      throw new Error(
+        `El producto con el id ${id} no se encuentra en nuestra base de datos.`
+      );
+    } else {
+      updateProduct.stock = stock || updateProduct.stock;
+      updateProduct.availability = availability || updateProduct.availability;
+      updateProduct.title = title || updateProduct.title;
+      updateProduct.price = price || updateProduct.price;
+      updateProduct.detail = detail || updateProduct.detail;
+      updateProduct.mainImage = mainImage || updateProduct.mainImage;
+      updateProduct.images = images || updateProduct.images;
+      await updateProduct.save();
+      return {
+        message: `El producto con el id ${id} se ha actualizado con éxito.`,
+        data: updateProduct,
+      };
+    }
   }
 
   //* Borrar un producto.
