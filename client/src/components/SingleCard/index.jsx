6f1ddc80401f01d44866fe5a391c9d5a9f83b705
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-24 23:14:39 -0300

    Update index.jsx
    .

--- client/src/components/SingleCard/index.jsx
+++ client/src/components/SingleCard/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   Button,
   CardContent,
@@ -8,9 +9,11 @@ import {
   CardActions,
 } from '@mui/material';
 
-const SingleCard = ({ id, name, price, mainImage }) => {
+import ButtonComponent from '../Button';
+
+const SingleCard = ({ id, name, price, mainImage, maxWidth }) => {
   return (
-    <Card sx={{ maxWidth: '100%' }}>
+    <Card sx={{ maxWidth }}>
       <CardMedia component='img' image={mainImage} height='200' alt={name} />
 
       <CardContent>
@@ -21,10 +24,28 @@ const SingleCard = ({ id, name, price, mainImage }) => {
       </CardContent>
 
       <CardActions>
-        <Button variant='outlined'>ADD CARD</Button>
+        <ButtonComponent
+          variant='outlined'
+          size='medium'
+          text='ADD CARD'
+          onClick={() => {}}
+        >
+          ADD CARD
+        </ButtonComponent>
       </CardActions>
     </Card>
   );
+};
+
+SingleCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  mainImage: PropTypes.string.isRequired,
+  maxWidth: PropTypes.string,
+};
+
+ButtonComponent.defaultProps = {
+  maxWidth: '100%',
 };
 
 export default SingleCard;
