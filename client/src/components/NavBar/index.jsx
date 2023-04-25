5a2d04e361b6da2ef2971a3cce6d07426378f005
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-24 23:14:04 -0300

    Update index.jsx
    Componente NavBar

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -1,19 +1,121 @@
-import LinkRouter from "../Link";
-import styles from "./NavBar.module.css";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Box,
+  Switch,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import LinkRouter from '../LinkRouter';
+import ButtonComponent from '../Button';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import ButtonTheme from '../ButtonTheme';
 
-const NavBar = () => {
+const NavBar = ({ handleThemeChange }) => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <nav className={styles.container}>
-      <p>LOGO</p>
+    <>
+      <AppBar
+        component='nav'
+        position='static'
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography variant='h5'>E-LOLEROS</Typography>
 
-      <div className={styles.buttonsNav}>
-        <LinkRouter to="home"/>
-        <LinkRouter to="about"/>
-        <LinkRouter to="shop"/>
-        <LinkRouter to="footer" value="contactos"/>
-      </div>
-    </nav>
+          <IconButton
+            color='inherit'
+            size='large'
+            onClick={() => setOpen(true)}
+            sx={{ display: { xs: 'flex', sm: 'flex', md: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+
+          <Box sx={{ display: { xs: 'none', sm: 'none', md: 'flex' } }}>
+            <LinkRouter to='home' />
+            <LinkRouter to='shop' />
+            <LinkRouter to='about' />
+            <LinkRouter to='login' />
+            <LinkRouter to='singin' value='Sing In' />
+            <Switch onChange={handleThemeChange} />
+          </Box>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer
+        open={open}
+        anchor='right'
+        onClose={() => setOpen(false)}
+        sx={{
+          display: { xs: 'block', sm: 'block', md: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: '60vw' },
+        }}
+      >
+        <Box sx={{ p: 2 }}>
+          <List>
+            <ButtonTheme />
+            <ListItem
+              button
+              component={Link}
+              to='home'
+              onClick={() => setOpen(false)}
+            >
+              <ListItemText primary='Home' />
+            </ListItem>
+            <ListItem
+              button
+              component={Link}
+              to='shop'
+              onClick={() => setOpen(false)}
+            >
+              <ListItemText primary='Shop' />
+            </ListItem>
+            <ListItem
+              button
+              component={Link}
+              to='about'
+              onClick={() => setOpen(false)}
+            >
+              <ListItemText primary='About' />
+            </ListItem>
+            <ListItem
+              button
+              component={Link}
+              to='login'
+              onClick={() => setOpen(false)}
+            >
+              <ListItemText primary='Contact' />
+            </ListItem>
+            <ListItem
+              button
+              component={Link}
+              to='singin'
+              onClick={() => setOpen(false)}
+            >
+              <ListItemText primary='Contact' />
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
+    </>
   );
+};
+
+NavBar.propTypes = {
+  handleThemeChange: PropTypes.func,
+};
+
+ButtonComponent.defaultProps = {
+  handleThemeChange: undefined,
 };
 
 export default NavBar;
