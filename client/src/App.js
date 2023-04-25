0e105f18145fc1be12e1d2e0d66e3a18591761c9
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-04-25 14:42:27 -0300

    Update App.js
    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,35 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from './views/HomePage';
 import LandingPage from './views/LandingPage';
 import NavBar from './components/NavBar';
+import { ThemeProvider } from '@mui/material/styles';
+import { useState } from 'react';
+import { lightTheme, darkTheme } from './themes/themes';
+import { Box, CssBaseline } from '@mui/material';
 
 const App = () => {
+  const location = useLocation();
+  const [theme, setTheme] = useState(lightTheme);
+
+  const handleThemeChange = () => {
+    setTheme(theme === lightTheme ? darkTheme : lightTheme);
+  };
+
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route path='/home' element={<HomePage />} />
-        <Route path='/' element={<LandingPage />} />
-      </Routes>
+      {/* <CssBaseline /> */}
+      <ThemeProvider theme={theme}>
+        {location.pathname === '/' ? (
+          <LandingPage />
+        ) : (
+          <NavBar handleThemeChange={handleThemeChange} />
+        )}
+
+        <Routes>
+          <Route path='/home' element={<HomePage theme={theme} />} />
+        </Routes>
+      </ThemeProvider>
     </>
   );
 };
