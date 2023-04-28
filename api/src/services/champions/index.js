6300941c6e2a3e7984b72f17afaf023dcae27ebd
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-04-27 21:54:10 -0300

    Buscar por name

--- api/src/services/champions/index.js
+++ api/src/services/champions/index.js
@@ -5,20 +5,33 @@ const championsModel = require('../../libs/database/models/champions');
 class ChampionsService {
   constructor() {}
 
-  //* Obtener todos los productos.
+  //* Obtener todos los champions.
   async findAll() {
-    const response = await championsModel.findAll();
-    if (response.length === 0) {
+    const findAllChampions = await championsModel.findAll();
+    if (findAllChampions.length === 0) {
       throw new Error('La base de datos está vacía.');
     } else {
-      return response;
+      return findAllChampions;
     }
   }
 
-  //* Obtener producto por id.
-  async findOne(id) {}
+  //* Obtener champion por name.
+  async findOne(name) {
+    const validateString = parseInt(name);
+    if (validateString) {
+      throw new Error(
+        `El nombre del campeón no es válido debido a que ${name} es un número.`
+      );
+    }
+    const findOneChampion = await championsModel.findByPk(name);
+    if (findOneChampion === null) {
+      throw new Error(`No existe el campeón ${name} en nuestra base de datos.`);
+    } else {
+      return findOneChampion;
+    }
+  }
 
-  //* Crear un producto.
+  //* Crear un champion.
   async create({ name, image, role, difficulty, history, skills, skins }) {
     const validateName = await championsModel.findByPk(name);
     if (validateName) {
@@ -68,10 +81,10 @@ class ChampionsService {
     };
   }
 
-  //* Editar un producto.
+  //* Editar un champion.
   async update() {}
 
-  //* Borrar un producto.
+  //* Borrar un champion.
   async delete(id) {}
 }
 
