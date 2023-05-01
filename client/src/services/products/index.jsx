bcb8127150532b924dd3fc2050218dbfb46682a7
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-05-01 20:44:08 -0300

    Create index.jsx
    Se creó un custom hook para la petición de get all products

---
+++ client/src/services/products/index.jsx
@@ -0,0 +1,25 @@
+import instance from '../../config';
+import { useQuery } from '@tanstack/react-query';
+
+const getAllProducts = async ({ queryKey }) => {
+  const [_key, page] = queryKey;
+
+  const response = await instance.get(`/products`, {
+      params: {
+        page
+      }
+    }
+  );
+
+  return response.data;
+  // throw new Error('Holis');
+};
+
+export const useAllProducts = (currentPage) => {
+
+  const query = useQuery(['products', currentPage], getAllProducts, {
+    keepPreviousData: true,
+  });
+
+  return query; 
+}
