1c504794855061370d11c704d777a1243456512f
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-05-01 12:51:12 -0300

    Google

---
+++ api/src/routes/login/index.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+const LoginController = require('../../controllers/login');
+const router = Router();
+const controller = new LoginController();
+
+//* Obtener todos los campeones.
+router.get('/', controller.validateToken);
+
+module.exports = router;
