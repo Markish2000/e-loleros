66b50a1fc70850d453bddcbfea0ca1f01a764512
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-05-01 20:44:29 -0300

    Update index.jsx
    Se reemplazó por custom hook useAllProduct

--- client/src/views/ShopPage/index.jsx
+++ client/src/views/ShopPage/index.jsx
@@ -1,35 +1,21 @@
 import StartPage from '../../components/StartPage';
 import image from '../../assets/productsStart.jpg';
 import Cards from '../../components/Cards';
-import { Container, Pagination, Box } from '@mui/material';
-import axios from 'axios';
+import { Pagination, Box } from '@mui/material';
 import { useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { useAllProducts } from '../../services/products';
 
 const ShopPage = () => {
   const text =
     'Sólo tu puedes oírme invocador ¿Qué obra maestra vamos a tocar hoy? -Sona';
 
-  const getAllProducts = async ({ queryKey }) => {
-    const [_key, page] = queryKey;
-
-    const response = await axios.get(
-      `http://localhost:3001/api/v1/products?page=${page}`
-    );
-
-    return response.data;
-    // throw new Error('Holis');
-  };
-
   const [currentPage, setCurrentPage] = useState(1);
+
+  const query = useAllProducts(currentPage)
 
   const handlePageChange = (event, page) => {
     setCurrentPage(page);
   };
-
-  const query = useQuery(['products', currentPage], getAllProducts, {
-    keepPreviousData: true,
-  });
 
   if (query.error) {
     return <div>{query.error.message}</div>;
