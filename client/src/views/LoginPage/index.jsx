aab9447d2cab96db9568a151f257cc4995f8e805
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-05-04 14:03:54 -0300

    .

--- client/src/views/LoginPage/index.jsx
+++ client/src/views/LoginPage/index.jsx
@@ -2,8 +2,9 @@ import { Box, Container } from '@mui/material';
 
 const LoginPage = () => {
   return (
-    <Box>
-      <h2>Holis</h2>
+    <Box sx={{ height: '100vh' }}>
+      <Box>Holis</Box>
+      <Box>Holis</Box>
     </Box>
   );
 };
