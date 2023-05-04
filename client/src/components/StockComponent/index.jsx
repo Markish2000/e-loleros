560a90fee8348899731eaf63d81dc77285aa7518
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-05-03 23:41:02 -0300

    Se modificaron cosas del componente Detail

---
+++ client/src/components/StockComponent/index.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { Typography, Button, IconButton } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+
+const StockComponent = ({ maxStock }) => {
+  const [quantity, setQuantity] = useState(0);
+
+  const handleIncrease = () => {
+    if (quantity < maxStock) {
+      setQuantity(quantity + 1);
+    }
+  };
+
+  const handleDecrease = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  return (
+    <div>
+      <Typography variant='h5' color='secondary' sx={{ marginBottom: '1rem' }}>
+        Stock disponible: {maxStock}
+      </Typography>
+      <Typography variant='h6' sx={{ marginBottom: '1rem' }}>
+        Cantidad: {quantity}
+      </Typography>
+      <Button
+        variant='outlined'
+        color='primary'
+        onClick={handleIncrease}
+        disabled={quantity >= maxStock}
+        sx={{ marginRight: '0.75rem' }}
+      >
+        <AddIcon />
+      </Button>
+
+      <Button
+        variant='outlined'
+        color='primary'
+        onClick={handleDecrease}
+        disabled={quantity <= 0}
+      >
+        <RemoveIcon />
+      </Button>
+    </div>
+  );
+};
+
+export default StockComponent;
