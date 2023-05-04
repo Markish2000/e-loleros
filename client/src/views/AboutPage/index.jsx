aa4aa2b1b322a881286b55c4acabcbb34dee7c91
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-05-04 13:57:47 -0300

    Se agregró el start de about

--- client/src/views/AboutPage/index.jsx
+++ client/src/views/AboutPage/index.jsx
@@ -1,9 +1,19 @@
 import { Box, Container } from '@mui/material';
+import StartPage from '../../components/StartPage';
+import { textAbout } from '../../texts/startText';
+import imageDark from '../../assets/aboutDark.jpg';
+import imageLight from '../../assets/aboutLight.jpg';
+import { useThemeContext } from '../../context/ThemeContext';
 
 const AboutPage = () => {
+  const theme = useThemeContext();
   return (
     <Box>
-      <h1>holis</h1>
+      <StartPage
+        title='Nosotros'
+        text={textAbout}
+        image={theme.palette.mode === 'light' ? imageLight : imageDark}
+      />
     </Box>
   );
 };
