0af8308bc2338ec768bb20adfdb5d429df40c1a9
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-05-17 20:14:50 -0300

    Se cambió el diseño de champions

--- client/src/components/CardsChampions/index.jsx
+++ client/src/components/CardsChampions/index.jsx
@@ -6,10 +6,10 @@ import SingleCardChampions from '../SingleCardChampions/index.jsx';
 const CardsChampions = ({ data }) => {
   return (
     <Container>
-      <Grid container spacing={2}>
+      <Grid container spacing={{ xs: 3, sm: 3, md: 3, lg: 2 }}>
         {data.length !== 0
           ? data.map((el) => (
-              <Grid item key={el.id} xs={12} sm={6} md={4}>
+              <Grid item key={el.id} xs={12} sm={12} md={4} lg={3}>
                 <SingleCardChampions {...el} />
               </Grid>
             ))
